c902a622e82d15e2704bb0f102248ab669339d1e
Swaraj <darekar138@gmail.com>
2026-02-14 21:17:59 +0530

    scope update

--- app/routes/GoogleIntegra.py
+++ app/routes/GoogleIntegra.py
@@ -17,6 +17,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/generative-language.peruserquota",
+    "https://www.googleapis.com/auth/generative-language.retriever"
 ]
 
 @google_auth_bp.route("/api/auth/google/login")
